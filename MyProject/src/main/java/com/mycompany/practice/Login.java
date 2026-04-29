23b91dc9faccd1ef9b963933d80b403f326a4114
Arjay <arjaylunadagso@gmail.com>
2026-04-29 17:23:50 +0800

    Update Login.java

--- MyProject/src/main/java/com/mycompany/practice/Login.java
+++ MyProject/src/main/java/com/mycompany/practice/Login.java
@@ -1,4 +1,4 @@
-package com.mycompany.practice;
+package com.mycompany.myproject;
 
 import java.awt.Color;
 import java.awt.GridBagLayout;
@@ -10,11 +10,13 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import org.mindrot.jbcrypt.BCrypt;
 
-public class Login extends JFrame {
+public class Login_Frame extends JFrame {
 
     private Login_Panel login;
+    private UserController controller = new UserController();
+    private User user;
 
-    public Login() {
+    public Login_Frame() {
         setSize(1200, 800);
         setTitle("Login");
         setLocationRelativeTo(null);
@@ -34,54 +36,23 @@ public class Login extends JFrame {
         });
 
         login.getLoginBtn().addActionListener(e -> {
-            if (login()) {
-                JOptionPane.showMessageDialog(this, "Login successfully!");
-            } else {
-                JOptionPane.showMessageDialog(this, "Wrong credentials.");
-                login.getUsernameField().setText("");
-                login.getPasswordField().setText("");
-            }
+            setUpAction();
         });
     }
 
-    private boolean login() {
+    private void setUpAction() {
 
         String username = login.getUsernameField().getText();
-        char[] passwordChar = login.getPasswordField().getPassword();
+        String password = new String(login.getPasswordField().getPassword());
 
-        if (username.isEmpty()) {
-            JOptionPane.showMessageDialog(this, "Username field is empty!");
-            return false;
+        User user = new User(username, password);
+
+        String result = controller.login(user);
+
+        if (result.equals("SUCCESS")) {
+            JOptionPane.showMessageDialog(this, "Login successfully!");
+        } else {
+            JOptionPane.showMessageDialog(this, result);
         }
-
-        if (passwordChar.length == 0) {
-            JOptionPane.showMessageDialog(this, "Password field is empty!");
-            return false;
-        }
-
-        Connection connection = DBConnection.getConnection();
-        if (connection != null) {
-            System.out.println("Connected");
-        }
-
-        String sql = "SELECT * FROM users WHERE username = ?";
-
-        try (PreparedStatement ps = connection.prepareStatement(sql)) {
-            ps.setString(1, username);
-            ResultSet rs = ps.executeQuery();
-
-            if (rs.next()) {
-                String check = rs.getString("Password");
-                String inputPassword = new String(passwordChar);
-
-                if (BCrypt.checkpw(inputPassword, check)) {
-                    return true;
-                }
-            }
-        } catch (SQLException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-
-        return false;
     }
 }
